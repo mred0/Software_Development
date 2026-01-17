2f20210fbb4b1ec6303f2d5991ecd9047d66c38a
Hemang Rathod «⟨мя.ꍟꀸ⟩» <101561859+mred0@users.noreply.github.com>
2026-01-17 03:00:14 +0000

    Created Task Code File For Unit 1

--- CONSOLIDATE/Unit 1/Unit1.py
+++
@@ -1,1 +0,0 @@
-In Consolidate File You Will Find All Course Work We Did Weekly Based In Classes As Well As Practice.
---
+++ CONSOLIDATE/Unit 1/tasks.py
@@ -0,0 +1,10 @@
+# Task 1 - Here We Are Creating Our First Hello World Programme.
+# We Used Online Compiler To Run This Code
+print("Hello World")
+
+# Task 2 - For Practice And Go Beyond Hello World We Print Many Other Lines Of Code.
+print("Welcome to the Software Development Unit 1") 
+
+print("Hello, my name is Hemang Rathod") 
+
+print("We Are Learning Python Here, and We Are really enjoying it!") 
